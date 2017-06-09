665c16f1bbe954434a7655042284e6fca3e190ac
Jari Verhaard <wbjari@gmail.com>
2017-06-09 12:48:15 +0200

    Frontend HTML

---
+++ frontend/overview.php
@@ -0,0 +1,20 @@
+<?php include("header.php"); ?>
+<div class="row stream-overview">
+  <div class="row">
+    <div class="col-md-12 bar">
+      <span class="sidebar-title">
+        <span>Live right now</span>
+      </span>
+    </div>
+  </div>
+  <div class="col-md-3 stream-item">
+    <a href="#">
+      <div class="thumb">
+        <span>[Name streamer]</span>
+        <!-- IMAGE -->
+      </div>
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+    </a>
+  </div>
+</div>
+<?php include("footer.php"); ?>
---
+++ frontend/stream.php
@@ -0,0 +1,106 @@
+<?php include("header.php"); ?>
+<div class="row">
+    <div class="col-xs-2 sidebar">
+      <div class="row">
+      <div class="container-fluid bar">
+        <span class="sidebar-title">
+          <span>Streams</span>
+        </span>
+      </div>
+    </div>
+    <a href="#" class="stream-link">
+      <div class="thumb">
+        <span class="stream-link-name">[Name streamer]</span>
+        <!-- IMAGE -->
+      </div>
+      <span class="stream-link-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+    </a>
+    <a href="#" class="stream-link">
+      <div class="thumb">
+        <span class="stream-link-name">[Name streamer]</span>
+        <!-- IMAGE -->
+      </div>
+      <span class="stream-link-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+    </a>
+    <a href="#" class="stream-link">
+      <div class="thumb">
+        <span class="stream-link-name">[Name streamer]</span>
+        <!-- IMAGE -->
+      </div>
+      <span class="stream-link-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+    </a>
+  </div>
+  <div class="col-xs-7 stream">
+    <div class="row">
+      <div class="container-fluid bar">
+        <span class="sidebar-title">
+          <a href="#">[Name streamer]</a>
+        </span>
+      </div>
+    </div>
+    <div class="video-container">
+      <div class="row">
+        <div class="col-xs-6">
+          <video src="https://08-lvl3-pdl.vimeocdn.com/01/4385/1/46926279/110968829.mp4?expires=1496925441&token=06013cc5527f7a932a89a" autoplay></video>
+          <span class="stream-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <a href="#">[Name streamer]</a>
+        </div>
+        <div class="col-xs-6">
+          <video src="https://08-lvl3-pdl.vimeocdn.com/01/4385/1/46926279/110968829.mp4?expires=1496925441&token=06013cc5527f7a932a89a" autoplay></video>
+          <span class="stream-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <a href="#">[Name streamer]</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-6">
+          <video src="https://08-lvl3-pdl.vimeocdn.com/01/4385/1/46926279/110968829.mp4?expires=1496925441&token=06013cc5527f7a932a89a" autoplay></video>
+          <span class="stream-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <a href="#">[Name streamer]</a>
+        </div>
+        <div class="col-xs-6">
+          <video src="https://08-lvl3-pdl.vimeocdn.com/01/4385/1/46926279/110968829.mp4?expires=1496925441&token=06013cc5527f7a932a89a" autoplay></video>
+          <span class="stream-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <a href="#">[Name streamer]</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-3 chat">
+    <div class="row">
+      <div class="container-fluid bar">
+        <span class="sidebar-title">
+          <span>Chat : [Name streamer]</span>
+        </span>
+      </div>
+    </div>
+    <div class="chatbox">
+      <div class="bubble">
+        <a href="#">[Name user]</a>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </div>
+      <div class="bubble">
+        <a href="#">[Name user]</a>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </div>
+      <div class="bubble">
+        <a href="#">[Name user]</a>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </div>
+      <div class="bubble">
+        <a href="#">[Name user]</a>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </div>
+      <div class="bubble">
+        <a href="#">[Name user]</a>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </div>
+    </div>
+    <div class="container-fluid chat-form">
+      <form class="" action="index.html" method="post">
+        <input class="form-control" id="focusedInput" name="chat-input" type="text" placeholder="Type to chat..">
+        <input class="btn btn-success chat-button" name="chat-button" type="button" value="Send">
+      </form>
+    </div>
+  </div>
+</div>
+<?php include("footer.php"); ?>
