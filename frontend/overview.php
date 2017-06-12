7c136221872f7d35f0644ce096c9319fff5a99bd
Jari Verhaard <wbjari@gmail.com>
2017-06-12 10:27:47 +0200

    Streamers overzicht

--- frontend/overview.php
+++ frontend/overview.php
@@ -1,20 +1,93 @@
 <?php include("header.php"); ?>
 <div class="row stream-overview">
-  <div class="row">
-    <div class="col-md-12 bar">
-      <span class="sidebar-title">
-        <span>Live right now</span>
-      </span>
-    </div>
+  <div class="container-fluid bar">
+    <span class="sidebar-title">
+      <span>Live right now</span>
+    </span>
   </div>
-  <div class="col-md-3 stream-item">
-    <a href="#">
-      <div class="thumb">
-        <span>[Name streamer]</span>
-        <!-- IMAGE -->
-      </div>
-      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
-    </a>
+  <div class="stream-items">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 stream-item">
+      <a href="#">
+        <div class="thumb">
+          <span>[Name streamer]</span>
+          <!-- IMAGE -->
+        </div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+      </a>
+    </div>
+    <div class="clearfix"></div>
   </div>
 </div>
 <?php include("footer.php"); ?>
