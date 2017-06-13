920b383ed651c2dd114d8e562520377f2084b28b
Hugo Zink <hugo.zink@hotmail.com>
2017-06-13 10:37:13 +0200

    Finalized signature verification.

--- js/stream.js
+++ js/stream.js
@@ -1,42 +1,126 @@
-// Video Data 
-var videoSocket = new WebSocket('ws://145.49.35.215:1234/receive');
-// Video Queue
-var videoQueue = [];
-// Video Player
-var videoPlayer = document.getElementById('videoplayer');
-
-var playing = false;
-
-// When the connection is open, send some data to the server
-videoSocket.onopen = function() {
-    videoSocket.send('Ping'); // Send the message 'Ping' to the server
-};
-// Log errors
-videoSocket.onerror = function(error) {
-    console.log('WebSocket Error ' + error);
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
 };
 
-// Stop Video when ended
-videoPlayer.addEventListener('ended', function() {
-    playing = false;
-    playVideo();
-}, false);
+$(document).ready(function(){
 
-// Play Video on Data receive
-videoSocket.onmessage = function(event) {
-    console.log(event.data);
-    videoQueue.push(URL.createObjectURL(event.data));
+    // Video Data
+    var videoSocket = new WebSocket('ws://localhost:1234/receive');
+    // Video Queue
+    var videoQueue = [];
+    // Video Player
+    var videoPlayer = document.getElementById('videoplayer');
 
-    if(!playing) {
+    var playing = false;
+
+    var pubKey;
+
+    var lastVideo;
+
+    // When the connection is open, send some data to the server
+    videoSocket.onopen = function() {
+        //Ask for public key from server
+        videoSocket.send('PUBKEY');
+    };
+
+    //Log errors
+    videoSocket.onerror = function(error) {
+        console.log('WebSocket Error ' + error);
+    };
+
+    //Play next fragment when the video ends.
+    videoPlayer.addEventListener('ended', function() {
+        playing = false;
         playVideo();
-    }
-};
+    }, false);
 
-// Start Video
-function playVideo() {
-    if(videoQueue.length > 0) {
-        playing = true;
-        videoPlayer.src = videoQueue.shift();
-        videoPlayer.load();
+    //Receive video and play fragment.
+    videoSocket.onmessage = function(event) {
+        //If data is a blob, it's probably a video fragment.
+        if(event.data instanceof Blob)
+        {
+            lastVideo = event.data;
+
+            videoQueue.push(URL.createObjectURL(event.data));
+
+            if(!playing)
+            {
+                playVideo();
+            }
+        }
+        else
+        {
+            //Indicates we have received the public key
+            if(event.data.startsWith('-----BEGIN PUBLIC KEY-----'))
+            {
+                /*
+                 //Remove begin/end public key lines.
+                 pubKey = event.data;
+                 pubKey = pubKey.split("\n");
+                 pubKey.shift();
+                 pubKey.pop();
+
+                 pubKey = pubKey.join("\n");
+                 */
+
+                pubKey = event.data;
+            }
+            else if(event.data.startsWith("SIGN:"))
+            {
+                checkHash(event.data.substr(6));
+            }
+            else if(event.data.startsWith("EXPECT:"))
+            {
+                console.log(event.data);
+            }
+        }
+    };
+
+    //If a video is queued, play it.
+    function playVideo()
+    {
+        if(videoQueue.length > 0)
+        {
+            playing = true;
+            videoPlayer.src = videoQueue.shift();
+            videoPlayer.load();
+        }
     }
-}
+
+    function checkHash(encryptedHash)
+    {
+        var base64data;
+
+        var reader = new window.FileReader();
+
+        //Convert blob to base64.
+        reader.readAsDataURL(lastVideo);
+        reader.onloadend = function() {
+            base64data = reader.result;
+
+            base64data = base64data.substr(base64data.indexOf(',') + 1);
+
+            //Initialize signature checkers
+            var sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
+
+            sig.init(pubKey); // signer's certificate
+            // update data
+            sig.updateString(base64data);
+
+            // verify signature
+            if(!sig.verify(encryptedHash))
+            {
+                console.log("Signature verification failed!");
+
+                var html = `<div class="alert">
+                                <p>Possible tampering detected! Please reload the page to try again.</p>
+                            </div>`;
+
+                $(html).appendTo(document.body);
+            }
+        };
+    }
+
+});
