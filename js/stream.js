946c6d0be766d4aadd59fde6bf0cb1fd6b8f463f
Hugo Zink <hugo.zink@hotmail.com>
2017-06-14 12:17:21 +0200

    Added support for multiple streams (up to 4).

--- js/stream.js
+++ js/stream.js
@@ -1,162 +1,67 @@
-// Video Data
-var videoSocket;
-// Video Queue
-var videoQueue = [];
-// Video Player
-var videoPlayer = document.getElementById('videoplayer');
-var streamsList = document.getElementById('streamslist');
+var videoPlayers = [];
 
-var playing = false;
+videoPlayers[1] = new VideoPlayer(document.getElementById('videoplayer1'));
+videoPlayers[1].openWebSocket();
 
-var pubKey;
+var streamsList = document.getElementsByClassName('streamslist');
 
-var lastVideo;
+//Open a websocket for a list of streams
+var listSocket = new WebSocket("ws://localhost:1234/receive");
+
+listSocket.onopen = function(){
+    //Register a function to check for a list of streams
+    setInterval(getStreamList, 10000);
+
+    getStreamList();
+};
+
+listSocket.onmessage = function(event){
+    let streams;
+    //Probably JSON, try parsing it
+    try{
+        streams = JSON.parse(event.data);
+    }
+    catch(e){
+        return;
+    }
+
+    $(streamsList).html('');
+
+    for(let stream of streams)
+    {
+        let li = document.createElement('li');
+        li.class = 'stream';
+        li.dataset.streamid = stream.id;
+        li.innerHTML = '<a href="#">'+ stream.id +'</a>';
+
+        for(let streamlist of streamsList)
+        {
+            streamlist.appendChild(li.cloneNode(true));
+        }
+    }
+};
 
 //Event handlers
-$(document).on('click', '#streamslist > li', function(e){
+$(document).on('click', '.streamslist > li', function(e){
 
-    switchStream($(this).data('streamid'));
+    switchStream($(this).data('streamid'), Number($(this).closest('div.videoplayercontainer').data('player')));
 
 });
 
-function openWebSocket()
-{
-    videoSocket = new WebSocket('ws://localhost:1234/receive');
-
-    videoSocket.onopen = function() {
-        //Ask for public key from server
-        videoSocket.send('PUBKEY');
-        videoSocket.send('LIST');
-
-        //Register a function to check for a list of streams
-        setInterval(getStreamList, 10000);
-    };
-
-    //Receive video and play fragment.
-    videoSocket.onmessage = function(event) {
-        //If data is a blob, it's probably a video fragment.
-        if(event.data instanceof Blob)
-        {
-            lastVideo = event.data;
-
-            videoQueue.push(URL.createObjectURL(event.data));
-
-            if(!playing)
-            {
-                playVideo();
-            }
-        }
-        else
-        {
-            //Indicates we have received the public key
-            if(event.data.startsWith('-----BEGIN PUBLIC KEY-----'))
-            {
-                pubKey = event.data;
-            }
-            else if(event.data.startsWith("SIGN:"))
-            {
-                checkHash(event.data.substr(6));
-            }
-            else if(event.data.startsWith("EXPECT:"))
-            {
-                console.log(event.data);
-            }
-            else
-            {
-                let streams;
-                //Probably JSON, try parsing it
-                try{
-                    streams = JSON.parse(event.data);
-                }
-                catch(e){
-                    return;
-                }
-
-                streamsList.innerHTML = '';
-
-                for(let stream of streams)
-                {
-                    let li = document.createElement('li');
-                    li.class = 'stream';
-                    li.dataset.streamid = stream.id;
-                    li.innerHTML = '<a href="#">'+ stream.id +'</a>';
-
-                    streamsList.appendChild(li);
-                }
-            }
-        }
-
-        //Log errors
-        videoSocket.onerror = function(error) {
-            console.log('WebSocket Error ' + error);
-        };
-
-        //Try to reconnect in 5 seconds
-        videoSocket.onclose = function(){
-            setTimeout(function(){openWebSocket()}, 5000);
-        };
-    };
-}
-
-openWebSocket();
-
-//Play next fragment when the video ends.
-videoPlayer.addEventListener('ended', function() {
-    playing = false;
-    playVideo();
-}, false);
-
-
-//If a video is queued, play it.
-function playVideo()
-{
-    if(videoQueue.length > 0)
-    {
-        playing = true;
-        videoPlayer.src = videoQueue.shift();
-        videoPlayer.load();
-    }
-}
-
-function checkHash(encryptedHash)
-{
-    let base64data;
-
-    let reader = new window.FileReader();
-
-    //Convert blob to base64.
-    reader.readAsDataURL(lastVideo);
-    reader.onloadend = function() {
-        base64data = reader.result;
-
-        base64data = base64data.substr(base64data.indexOf(',') + 1);
-
-        //Initialize signature checkers
-        let sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
-
-        sig.init(pubKey); // signer's certificate
-        // update data
-        sig.updateString(base64data);
-
-        // verify signature
-        if(!sig.verify(encryptedHash))
-        {
-            console.log("Signature verification failed!");
-
-            let alertBox = document.createElement('div');
-            alertBox.class = 'alert';
-            alertBox.innerHTML = '<p>Possible tampering detected! Please reload the page to try again.</p>';
-            document.body.appendChild(alertBox);
-        }
-    };
-}
-
 function getStreamList()
 {
-    videoSocket.send("LIST")
+    listSocket.send("LIST");
 }
 
-function switchStream(id)
+function switchStream(id, playerNumber)
 {
-    videoSocket.send('WATCH ' + id);
+    if(typeof videoPlayers[playerNumber] === "undefined")
+    {
+        videoPlayers[playerNumber] = new VideoPlayer(document.getElementById('videoplayer' + playerNumber));
+        videoPlayers[playerNumber].openWebSocket(id);
+    }
+    else
+    {
+        videoPlayers[playerNumber].watch(id);
+    }
 }
